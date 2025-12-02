4736f97deb0bc62291cee15203349592667d4e7b
Terry Ntuli <meriusterry>
2025-12-02 14:12:03 +0200

    updated UI

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -20,7 +20,7 @@ export default function Settings() {
         />
 
         <button className="bg-teal-600 text-white px-4 py-2 rounded hover:bg-teal-700">
-          Save Settings
+          Save
         </button>
       </div>
     </div>
